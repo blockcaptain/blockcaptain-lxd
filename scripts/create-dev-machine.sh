7e7ee3be7d66b1c367a3189bc620fd9a80ab1b2a
Will Brown <will@rebeagle.com>
2020-08-06 20:51:10 -0400

    update

--- scripts/create-dev-machine.sh
+++ scripts/create-dev-machine.sh
@@ -3,6 +3,9 @@ set -euo pipefail
 
 lxc init ubuntu/blkcapt "blkcaptdev" --vm
 for i in {1..3}; do 
+    if lxc storage volume show default "blkcaptdev-disk${i}"; then
+        lxc storage volume delete default "blkcaptdev-disk${i}"
+    fi
     lxc storage volume create default "blkcaptdev-disk${i}" size=1GiB --type=block
     lxc config device add "blkcaptdev" "blkcaptdev-disk${i}" disk source="blkcaptdev-disk${i}" pool=default
 done
