798b5fcfa8b8644c1b9a455d2eafd5ce4d67804d
Will Brown <will@rebeagle.com>
2020-12-28 22:11:52 -0500

    release flow

--- images/download-latest.sh
+++ images/download-latest.sh
@@ -17,5 +17,5 @@ fi
 ./ok.sh download_workflow_run_artifact "${REPO_OWNER}" "${REPO_NAME}" "${ARTIFACT_ID}" > ubuntu-vm.zip
 
 INFO=$(curl -H "Accept: application/vnd.github.v3+json" "https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/runs/${RUN_ID}")
-RELEASE=$(jq -r '.head_sha[0:12]+"-"+(.created_at | gsub("[-:]";""))' <<< $info)
+RELEASE=$(jq -r '.head_sha[0:12]+"-"+(.created_at | gsub("[-:]";""))' <<< $INFO)
 echo "::set-output name=release_name::$RELEASE"
